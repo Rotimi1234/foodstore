a75b8fc68b37cec0c3acfad9ecccfb26b7440d73
dami_codes <80917622+Rotimi1234@users.noreply.github.com>
2022-06-16 12:25:54 +0100

    Add files via upload

---
+++ pages/vegetables.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { client } from '../lib/client'
+
+import {Product, FooterBanner, HeroBanner} from '../components'
+
+const Home = ({products, bannerData}) => {
+  return (
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+ 
+
+      <div className="products-heading">
+        <h2>vegetable and Soups</h2>
+        </div>
+
+        <div className='products-container'>
+        {
+        products.filter(product => product.typeoffood=== "vegetable").map
+        ((product) => <Product key={product._id} product={product} />)}
+
+
+          {console.log(products)}
+      </div>
+      <FooterBanner footerBanner={bannerData[1]} />
+    </div>
+
+    
+  )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
+export default Home
